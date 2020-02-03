241d7ab9b4b56185f3bd11f5c2fd777f40163219
Jacob Berger <jberger.1919@gmail.com>
2020-02-03 13:10:13 -0800

    Notes for Graphs

---
+++ ClassExamples/src/Graphs/Graph.java
@@ -0,0 +1,23 @@
+package Graphs;
+
+public class Graph {
+	/*
+	 * Graph G = (V, E): a set of vertices V and a set of edges E; each edge in E is a connection between a pair of vertices in V, which are called adjacent vertices.
+	 * 	-undirected graph
+	 * 		Each vertex in the graph can be traversed in either direction through the edge
+	 * 	-directed graph
+	 * 		Each vertex can only be traversed in one direction through the edge
+	 * 	-weighted graph
+	 * 		Each edge has a numeric cost associated with it
+	 * 	-sparse graph
+	 * 		has few edges relative to the number of vertices
+	 * 	-dense graph
+	 * 		has many edges relative to the number of vertices
+	 * 	-complete graph
+	 * 		has all possible edges and can be directed or undirected
+	 * 
+	 * A path is simple if all vertices on the path are distinct
+	 * 
+	 */
+
+}
